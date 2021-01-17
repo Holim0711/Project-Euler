8eec82196b6f5f496682db05f302108922d004c0
Holim Lim <ihl7029@europa.snu.ac.kr>
2021-01-17 13:14:58 +0900

    more general function

--- 3/main.jl
+++ 3/main.jl
@@ -1,10 +1,25 @@
-function largest_prime_factor(n::Int)
-    for p in 2:round(Int, √n)
-        if n % p == 0
-            return largest_prime_factor(n ÷ p)
+function prime_factorization(n::Int)
+    result = []
+    for p in [2, 3]
+        while n % p == 0
+            push!(result, p)
+            n ÷= p
         end
     end
-    return n
+    for p = 6:6:ceil(Int, √n)
+        while n % (p-1) == 0
+            push!(result, (p-1))
+            n ÷= (p-1)
+        end
+        while n % (p+1) == 0
+            push!(result, (p+1))
+            n ÷= (p+1)
+        end
+    end
+    if n != 1
+        push!(result, n)
+    end
+    return result
 end
 
-println(largest_prime_factor(600851475143))
+println(pop!(prime_factorization(600851475143)))
