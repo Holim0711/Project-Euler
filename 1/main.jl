4d1c108f6e356e895bf7f864c92d1b414b151d25
Holim Lim <ihl7029@europa.snu.ac.kr>
2021-01-17 13:06:38 +0900

    inappropriate default parameters

--- 1/main.jl
+++ 1/main.jl
@@ -2,9 +2,13 @@ function sum_of_AP(d, n::Int)
     return d * (n * (n + 1) ÷ 2)
 end
 
-function sum_of_multiples(m, limit=1000)
+function sum_of_multiples(m, limit)
     n = (limit - 1) ÷ m
     return sum_of_AP(m, n)
 end
 
-println(sum_of_multiples(3) + sum_of_multiples(5) - sum_of_multiples(15))
+println(
+    sum_of_multiples(3, 1000)
+    + sum_of_multiples(5, 1000)
+    - sum_of_multiples(15, 1000)
+)
